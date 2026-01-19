95eb6acbac36f1f34a5d2f133c195d7a9eb87cbc
Lichao Huang <lhuang38@rrc.ca>
2026-01-18 22:22:07 -0600

    feat: test healthcheck

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,25 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
+interface HealthCheckResponse {
+    status: string;
+    uptime: number;
+    timestamp: string;
+    version: string;
+}
+
+
 app.get("/api/v1/health", (req: Request, res: Response) => {
-    res.json({
-        status: "OK",
-        uptime: process.uptime(),
-        timestamp: new Date().toISOString(),
-        version: "1.0.0",
-    });
+  const healthData: HealthCheckResponse = {
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  };
+
+  res.json(healthData);
 });
+
+
+
+export default app;
